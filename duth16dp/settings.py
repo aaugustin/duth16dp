c0e97206a68f7309a993860fa91b7d66bb0b4fb3
Aymeric Augustin <aymeric.augustin@m4x.org>
2016-10-29 00:07:53 +0200

    log database queries

--- duth16dp/settings.py
+++ duth16dp/settings.py
@@ -26,6 +26,22 @@ INSTALLED_APPS = [
 
 INTERNAL_IPS = ['127.0.0.1']
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
+
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
