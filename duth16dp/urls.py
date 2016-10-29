013b62c44776007115f2ae5856af4bbcf5b2e834
Aymeric Augustin <aymeric.augustin@m4x.org>
2016-10-29 12:29:02 +0200

    Put admin at the root URL.

--- duth16dp/urls.py
+++ duth16dp/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^__debug__/', include(debug_toolbar.urls)),
+    url(r'^', admin.site.urls),
 ]
